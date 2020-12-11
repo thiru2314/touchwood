735e3dd6dec133e1be1f5cb9c4c92d4bb950fcee
thiru <thiru.workmail@gmail.com>
2020-12-11 15:28:18 -0600

    Updated lombok dependency

--- touchwood/src/main/java/user/User.java
+++ touchwood/src/main/java/user/User.java
@@ -1,42 +1,14 @@
 package user;
 
+import lombok.*;
+
 import java.util.Date;
 
+@Data
+@AllArgsConstructor
 public class User {
 
     private Integer id;
-
     private String name;
-
     private Date birthDate;
-
-    public User(Integer id, String name, Date date) {
-        this.id = id;
-        this.name = name;
-        this.birthDate = date;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Date getDate() {
-        return birthDate;
-    }
-
-    public void setDate(Date date) {
-        this.birthDate = date;
-    }
 }
